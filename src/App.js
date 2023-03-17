5fc17c1930fc51fd0c9e96c6a03bebe925f83968
Mohamed Malek <mobile@matjrah.com>
2023-03-17 15:29:14 +0200

    Imported tailwind css & sortJs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import Sortable, { MultiDrag, Swap } from "sortablejs";
 
 function App() {
+  console.log(MultiDrag)
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="font-bold bg-sky-500 text-white">
         <p>sort your webstie</p>
       </header>
+      <main>
+        <div className="shadow-lg w-[500px] m-auto mt-[100px] p-[15px]">
+          <p>asdasd</p>
+          <ul id="items">
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
